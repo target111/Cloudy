c80c59a27d7eb1b9771a759df293e5f37deeb2f1
target_ <target111@protonmail.com>
2018-01-20 10:58:00 +0200

    Create utils.py

---
+++ Cloudy/src/utils.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+class ConsoleColors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+class PrintType(Enum):
+    Error = 1
+    Warning = 2
+    Info = 3
+
+def printEx(message, type):
+    print_type_str = ""
+    if type == PrintType.Error:
+        print_type_str = ConsoleColors.FAIL + "[ERROR]" + ConsoleColors.ENDC
+    elif type == PrintType.Warning:
+        print_type_str = ConsoleColors.WARNING + "[WARN]" + ConsoleColors.ENDC
+    elif type == PrintType.Info:
+        print_type_str = ConsoleColors.OKBLUE + "[INFO]" + ConsoleColors.ENDC
+
+    print(print_type_str + " " + message)
